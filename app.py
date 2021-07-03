961d2e64e727bec7eba1b824114392796cc03da6
Vaisak Nair <37840005+VaisakNair7@users.noreply.github.com>
2021-07-03 11:36:44 +0530

    Update app.py

--- app.py
+++ app.py
@@ -4,6 +4,7 @@ from flask import Flask, send_from_directory
 from pywebio.input import textarea
 from pywebio.output import *
 from pywebio import start_server
+import argparse
 
 import nltk
 import numpy as np
@@ -83,7 +84,14 @@ def predict():
 
 app.add_url_rule('/', 'webio_view', webio_view(predict), methods = ['GET', 'POST', 'OPTIONS'])
 
-app.run(host='localhost', port=80, debug = True)
+if __name__ == '__main__':
+    parser = argparse.ArgumentParser()
+    parser.add_argument("-p", "--port", type=int, default=8080)
+    args = parser.parse_args()
+
+    start_server(predict, port=args.port)
+
+#app.run(host='localhost', port=80, debug = True)
 
     
 
